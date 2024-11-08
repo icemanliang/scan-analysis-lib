32abd93710500b1401aa83a715603357f778ba0d
前端iceman <734099485@qq.com>
2024-11-08 16:17:47 +0800

    feat: update base info

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const validateConfig = require('./lib/validate');
 const Scanner = require('./lib/scanner');
 
+/**
+ * 扫描项目
+ * @param {Object} config 配置
+ * @returns {Promise<Object>} 扫描结果
+ */
+
 module.exports = async function scan(config = {}) {
   return new Promise(async (resolve, reject) => {
     try {
